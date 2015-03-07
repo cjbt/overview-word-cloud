627577e0c6a8112d760068a7046508c55ad6bde5
Ethan Resnick <ethan.resnick@gmail.com>
2015-03-06 23:04:41 -0500

    Add endpoints for getting/setting the cloud's excluded words

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 'use strict';
 var express            = require('express')
   , morgan             = require('morgan')
+  , bodyParser         = require('body-parser')
   , oboe               = require('./lib/oboe-requester')
   , API                = require('./lib/api')
   , DocSetTokenCounter = require('./lib/doc-set-token-counter')
@@ -10,6 +11,7 @@ var express            = require('express')
   , app                = express();
 
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 var nextRequestId = 1;
 
@@ -114,6 +116,45 @@ app.get('/show', function(req, res, next) {
 app.get('/metadata', function(req, res, next) {
   res.status(204).header('Access-Control-Allow-Origin', '*').send();
 })
+
+
+// Below, we read/write the hidden tokens to a separate StoreObject, not
+// the Store's main state, because doing so means we don't have to worry
+// about a race condition in which the client posts back some hidden
+// tokens before main state has been completely computed and saved (such
+// that saving the main state would override the hidden words).
+app.get('/hidden-tokens', function(req, res, next) {
+  var api = new API(req.query.server, req.query.apiToken)
+    , storeStream = api.getStoreObjects(oboe);
+
+  storeStream.done(function(storeObjects) {
+    if(storeObjects[0]) {
+      res.send(JSON.stringify(storeObjects[0].json));
+    }
+    else {
+      res.send('{"hidden-tokens": []}');
+    }
+  });
+});
+
+app.put('/hidden-tokens', function(req, res, next) {
+  var api = new API(req.query.server, req.query.apiToken)
+    , storeStream = api.getStoreObjects(oboe)
+    , storeData = {
+        "indexedString": "hidden tokens",
+        "json": {"hidden-tokens": req.body["hidden-tokens"]}
+      };
+
+  storeStream.done(function(storeObjects) {
+    if(storeObjects[0]) {
+      api.updateStoreObject(oboe, storeObjects[0].id, storeData);
+    }
+    else {
+      api.createStoreObject(oboe, storeData);
+    }
+    res.status(204).end();
+  });
+});
 
 app.use('/static', express.static(__dirname + '/public'));
 app.listen(process.env.PORT || 3000);
